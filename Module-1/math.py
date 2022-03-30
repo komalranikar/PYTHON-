f6d1e3d53a5fd9f8b3d0d53800a519251c1d49c4
Komal Rani Kar <93367596+komalranikar@users.noreply.github.com>
2022-03-30 07:15:56 +0000

    python programs

--- Module-1/math.py
+++ Module-1/math.py
@@ -1,3 +1,10 @@
 import math
 a=8.6
 print(math.ceil(a))
+print(math.floor(a))
+print(math.fabs(a))
+print(math.modf(a))
+print(math.sqrt(4))
+print(math.pow(2,3))
+print(math.fmod(2,3))
+print(math.trunc(a))
