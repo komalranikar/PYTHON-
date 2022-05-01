cce290cbdfc47bf4b6622aa9796bc5899850d01d
Komal Rani Kar <93367596+komalranikar@users.noreply.github.com>
2022-05-01 13:05:16 +0000

    Module-2 programs

---
+++ Module-2/str.py
@@ -0,0 +1,10 @@
+x=input("enter the statement:")
+#operations on string using unicodes
+print("The unicodes:",(id(x)))
+
+print("the length of the string:",len(x))
+#splitting of string
+print(x.split(input("enter char to splitting :")))
+
+#accessing   elements of string using locations
+print(x[int(input("enter the number:"))])
